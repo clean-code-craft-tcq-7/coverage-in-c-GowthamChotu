b1c8ed4819433ccea083df7a30fb2ee53eaa34d0
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2023-01-17 17:20:21 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,7 +7,6 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
-  REQUIRE(inferBreach(25, 20, 30) != TOO_LOW);
   
   //REQUIRE(TypesOfLimitCooling(PASSIVE_COOLING) == (coolingLimitType Limit= {0,35}));
   //REQUIRE(TypesOfLimitCooling(HI_ACTIVE_COOLING) == (coolingLimitType Limit= {0,45}));
@@ -16,7 +15,6 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 12) == 0);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 35) == 0);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 25) == 0);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 25) != 1);
   
   //REQUIRE(checkAndAlert(TO_CONTROLLER, {PASSIVE_COOLING, "Hello"}, 12));
   //REQUIRE(checkAndAlert(TO_EMAIL , {PASSIVE_COOLING, "Hello"}, 12));
