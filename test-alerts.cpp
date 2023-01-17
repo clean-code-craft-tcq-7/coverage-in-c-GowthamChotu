3904f067a44e557ec15676fb9a029329580d7885
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2023-01-17 17:16:47 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,6 +7,7 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(25, 20, 30) != TOO_LOW);
   
   //REQUIRE(TypesOfLimitCooling(PASSIVE_COOLING) == (coolingLimitType Limit= {0,35}));
   //REQUIRE(TypesOfLimitCooling(HI_ACTIVE_COOLING) == (coolingLimitType Limit= {0,45}));
